f033ff6d2d632d6ea7281408e15e473ec742fb87
Jean-François Corriveau <jcorriveau23@gmail.com>
2024-04-01 03:01:37 -0400

    update some of the game data model

--- crates/poolnhl_interface/src/daily_leaders/model.rs
+++ crates/poolnhl_interface/src/daily_leaders/model.rs
@@ -1,30 +1,12 @@
 use serde::{Deserialize, Serialize};
 
+#[allow(non_snake_case)]
 #[derive(Debug, Deserialize, Serialize)]
-pub struct DailyLeaders {
-    pub date: String,
-    pub goalies: Vec<DailyGoaly>,
-    pub skaters: Vec<DailySkater>,
-    pub played: Vec<u32>,
+pub struct SkaterStats {
+    pub assists: u8,
+    pub goals: u8,
+    pub shootoutGoals: u8,
 }
-
-#[derive(Debug, Deserialize, Serialize)]
-pub struct DailyGoaly {
-    pub name: String,
-    pub id: u32,
-    pub team: u32,
-    pub stats: GoalyStats,
-}
-
-#[derive(Debug, Deserialize, Serialize)]
-pub struct DailySkater {
-    pub name: String,
-    pub id: u32,
-    pub team: u32,
-    pub stats: SkaterStats,
-}
-
-// To reduce only the unused members have been commented out to reduce data usage.
 
 #[allow(non_snake_case)]
 #[derive(Debug, Deserialize, Serialize)]
@@ -35,12 +17,28 @@ pub struct GoalyStats {
     pub savePercentage: Option<f32>,
     pub OT: Option<bool>,
 }
-// To reduce only the unused members have been commented out to reduce data usage.
 
-#[allow(non_snake_case)]
 #[derive(Debug, Deserialize, Serialize)]
-pub struct SkaterStats {
-    pub assists: u8,
-    pub goals: u8,
-    pub shootoutGoals: u8,
+pub struct DailySkater {
+    pub name: String,
+    pub id: u32,
+    pub team: u32,
+    pub stats: SkaterStats,
+}
+
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct DailyGoaly {
+    pub name: String,
+    pub id: u32,
+    pub team: u32,
+    pub stats: GoalyStats,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct DailyLeaders {
+    pub date: String,
+    pub goalies: Vec<DailyGoaly>,
+    pub skaters: Vec<DailySkater>,
+    pub played: Vec<u32>,
 }
